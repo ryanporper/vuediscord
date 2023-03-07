c1812c493fd63b61fde2edb59c696bcee2e56ae4
ryanporper <ryan_porper@aol.com>
2023-03-06 21:53:45 -0500

    created reusabled layout for components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <script setup>
-import Navbar from './components/Navbar.vue';
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Invite from './components/Invite.vue';
-import Community from './components/Community.vue';
-import Tech from './components/Tech.vue';
-import Hanging from './components/Hanging.vue';
+import Navbar from "./components/Navbar.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Tech from "./components/Tech.vue";
+import Layout from "./components/Layout.vue";
+
+const layouts = [
+  {
+    img: "Img",
+    title: "Create an invite-only place where you belong",
+    description: "Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat."
+  },
+  {
+    img: "Img",
+    title: "Where hanging out is easy",
+    description: "Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call."
+  },
+  {
+    img: "Img",
+    title: "From few to a fandom",
+    description: "Get any community running with moderation tools and custom member access. Give members special powers, set up private channels, and more."
+  },
+];
+
 </script>
 
 <template>
   <Navbar />
   <Header />
-  <Invite />
-  <Hanging />
-  <Community />
+  <div v-for="(layout, index) in layouts" :key="index">
+    <Layout :img="layout.img" :title="layout.title" :description="layout.description" />
+  </div>
   <Tech />
   <Footer />
 </template>
-
---
+++ src/components/Layout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bg-white flex justify-center items-center text-start">
+    <div class="flex flex-col lg:flex-row lg:justify-center py-14 px-6 lg:py-32 lg:px-24">
+      <div class="h-72 w-full bg-red-600 lg:mr-20 rounded-2xl flex items-center justify-center">
+        {{ img }}
+      </div>
+      <div class="w-full">
+        <h2 class="text-xl lg:text-5xl font-bold my-6 ">
+          {{ title }}
+        </h2>
+        <p class="text-lg lg:text-xl">
+          {{ description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  props: {
+    img: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
